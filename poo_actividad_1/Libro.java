23878f2faab4a0766ff49a198a4c94eecde22997
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-13 18:45:49 -0500

    feat(libro): Añadir Constructores

--- poo_actividad_1/Libro.java
+++ poo_actividad_1/Libro.java
@@ -4,5 +4,19 @@ public class Libro{
     //En los atributos aplico el encapsulamiento poniendolos como privados
     private String titulo;
     private String autor;
-    private int numeroPaginas; 
+    private int numeroPaginas;
+
+    //1. Constructor por defecto
+    public Libro() {
+        this.titulo = "Cien años de soledad";
+        this.autor = "Gabriel García Márquez";
+        this.numeroPaginas = 471;
+    }
+
+    //2. Constructor parametrizado
+    public Libro(String titulo,String autor, int numeroPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
 }
