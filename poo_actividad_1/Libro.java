cd81b2801e8afeffb75775305f89fe8283e12fd2
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-13 18:40:47 -0500

    fear(libro): Crea clase Libro

---
+++ poo_actividad_1/Libro.java
@@ -0,0 +1,8 @@
+package repositorio_mjaller.poo_actividad_1;
+
+public class Libro{
+    //En los atributos aplico el encapsulamiento poniendolos como privados
+    private String titulo;
+    private String autor;
+    private int numeroPaginas; 
+}
