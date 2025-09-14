131f3f528e33b18bcaddda22d4e9f148075a5260
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-13 23:52:59 -0500

    feat(main): Crear Main

---
+++ poo_taller_protocoloindividual/Main.java
@@ -0,0 +1,10 @@
+package repositorio_mjaller.poo_taller_protocoloindividual;
+
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+
+}
