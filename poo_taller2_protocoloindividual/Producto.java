26c9209d40810fbc2fbe595ab1ed08af4042c49a
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-14 18:09:00 -0500

    feat(estudiante): Crear Clase Estudiante, constructores y metodo mostrar

---
+++ poo_taller2_protocoloindividual/Producto.java
@@ -0,0 +1,22 @@
+package repositorio_mjaller.poo_taller2_protocoloindividual;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public void mostrarProducto() {
+        System.out.println("""
+
+        Detalles del producto:
+        Nombre: %s
+        Precio: %s
+
+        """.formatted(nombre, precio));
+    }
+
+}
