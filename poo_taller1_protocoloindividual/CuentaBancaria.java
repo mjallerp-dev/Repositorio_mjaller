91410d60493cd13c4c1a9b8face30e12865a0232
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-13 19:29:29 -0500

    feat(cuentabancaria): Crear clase cuentaBancaria

---
+++ poo_taller1_protocoloindividual/CuentaBancaria.java
@@ -0,0 +1,9 @@
+package repositorio_mjaller.poo_taller1_protocoloindividual;
+
+public class CuentaBancaria {
+    //Propiedades
+    private String numeroCuenta; //Solo contiene numeros pero no es operable
+    private int saldo;
+    private String tipoCuenta;
+
+}
