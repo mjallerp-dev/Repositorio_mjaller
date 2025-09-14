5f243eb4ee04b8a38f739c4d4c238cc15b26ebfa
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-14 18:59:24 -0500

    feat(main): Entrega final taller 2 Protocolo Individual

--- poo_taller2_protocoloindividual/Main.java
+++ poo_taller2_protocoloindividual/Main.java
@@ -5,11 +5,13 @@ public class Main {
         
         Estudiante estudiante1 = new Estudiante("Miguel Jaller",26);
         Estudiante estudiante2 = new Estudiante();
+        Producto producto1 = new Producto("Jorge Rodriguez",28);
         System.out.println("\nUsando método estatico sin this: ");
         Estudiante.metodoEstatico(estudiante1);
         System.out.println("\nUsando método NO estático con this: ");
         estudiante2.metodoNoEstatico();
-
+        System.out.println("\nEjercicio 1: Clase Producto");
+        producto1.mostrarProducto();
     }
     
 }
