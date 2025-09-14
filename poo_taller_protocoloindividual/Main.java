cc95843e5bedb7d3d1f8d68313ddbe0a8de3b990
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-14 17:44:38 -0500

    feat(main): Entrega final Proyecto Integrador Taller 1 Protocolo Individual

--- poo_taller_protocoloindividual/Main.java
+++ poo_taller_protocoloindividual/Main.java
@@ -6,56 +6,81 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //Objeto de clase Libro
-        System.out.println("Inicializar objeto Libro \nIngrese los datos del libro: ");
-        System.out.println("Titulo: ");
-        String titulo = sc.nextLine();
-        System.out.println("Autor: ");
-        String autor = sc.nextLine();
-        System.out.println("Número de páginas: ");
-        int numeroPaginas = sc.nextInt();
+        //Objeto de clase Estudiante
+        System.out.println("\nBienvenido a la tienda online de la Biblioteca del colegio Comfenalco");
+        System.out.println("\nApreciado estudiante, para iniciar con la compra ingrese sus datos a continuación");
+        System.out.print("\nNombre y Apellido: ");
+        String nombre = sc.nextLine();
+        System.out.print("Edad: ");
+        int edad = sc.nextInt();
         sc.nextLine();
-        Libro libro1 = new Libro(titulo,autor,numeroPaginas);
-        Libro libro2 = new Libro("Cien años de soledad","Gabriel Garcia Marquez",471);
-        Libro libro3 = new Libro();
+        System.out.print("Curso (1 a 11): ");
+        int curso = sc.nextInt();
+        sc.nextLine();
+        Estudiante estudiante = new Estudiante(nombre, edad, curso);
+
+        
+        System.out.println("\nDatos recibidos correctamente");
 
         //Objeto de clase CuentaBancaria
-        System.out.println("Ingrese datos de la cuenta bancaria: ");
-        System.out.println("Número de cuenta (9 dígitos):");
+        System.out.println("\nIngrese datos de la cuenta bancaria: ");
+        System.out.print("\nNúmero de cuenta (9 dígitos):");
         String numeroCuenta = sc.nextLine();
             while (numeroCuenta.length() !=9) {
                 System.out.println("El numero de dígitos es inválido, por favor ingrese los 9 dígitos");
                 numeroCuenta = sc.nextLine();
             }
-        System.out.print("Saldo: ");
+        System.out.print("Saldo a recargar en su cuenta Biblioteca (min $10000): ");
         double saldo = sc.nextDouble();
         sc.nextLine();
         System.out.print("Tipo de cuenta: ");
         String tipoCuenta = sc.nextLine();
-        CuentaBancaria cuenta1 = new CuentaBancaria();
-        CuentaBancaria cuenta2 = new CuentaBancaria(numeroCuenta,tipoCuenta);
-        CuentaBancaria cuenta3 = new CuentaBancaria(numeroCuenta, saldo, "Ahorros");
+        CuentaBancaria cuenta1 = new CuentaBancaria(numeroCuenta,tipoCuenta);
+        cuenta1.setSaldo(saldo);
+        CuentaBancaria cuenta2 = new CuentaBancaria(numeroCuenta, saldo, "Ahorros");
+        
+        System.out.println("\nRecarga Exitosa: " + cuenta1);
 
-        //Objeto de clase Estudiante
-        System.out.println("\nIngrese datos del estudiante:");
-        System.out.print("Nombre: ");
-        String nombre = sc.nextLine();
-        System.out.print("Edad: ");
-        int edad = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Curso: ");
-        String curso = sc.nextLine();
-        Estudiante estudiante = new Estudiante(nombre, edad, curso);
+        //Objeto de clase Libro
+        System.out.println("\nIngrese los datos del libro que desea comprar: ");
+        System.out.print("\nTitulo del Libro: ");
+        String titulo = sc.nextLine();
+        System.out.print("Autor: ");
+        String autor = sc.nextLine();
+        double valorlibro = 10000;
+        System.out.println("Valor a paagar :" + valorlibro);      
+        int numeroPaginas;
+        while (true) { 
+        System.out.print("Número de páginas (Edición): ");
+            try {
+                numeroPaginas = Integer.parseInt(sc.nextLine());
+                if (numeroPaginas > 0) break;
+                    System.out.println("Debe ser mayor que 0.");
+                } catch (NumberFormatException e) {
+                    System.out.println("Debe ingresar un número válido.");
+                 }
+                }
+        Libro libro1 = new Libro(titulo,autor,numeroPaginas);
+        Libro libro2 = new Libro("Viaje al Centro de la Tierra","Julio Verne",1200);
+        Libro libro3 = new Libro();
 
         //Mostrar los objetos
-        System.out.println("\nDatos ingresados\n");
-        System.out.println(libro1);
-        System.out.println(libro2);
-        System.out.println(libro3);
-        System.out.println(cuenta1);
-        System.out.println(cuenta2);
-        System.out.println(cuenta3);
+        System.out.println("\nCompra Exitósa, datos de su compra: \n");
         System.out.println(estudiante);
+        System.out.println(libro1);
+        cuenta2.setSaldo(saldo-valorlibro);
+        System.out.println("\nDatos con saldo nuevo : \n" + cuenta2);
+
+        //Usando un text block
+        System.out.println("""            
+               
+            _____________________________________________________________
+            Tambien te podria interesar
+            %s
+            %s
+
+            Transaccioón Finalizada
+            """.formatted(libro2, libro3));
 
         sc.close();
 
