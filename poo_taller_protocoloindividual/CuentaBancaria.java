7538500d0b36dcfc5584ba2296fb015618562d0a
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-13 20:26:38 -0500

    feat(cuentabancaria): Mejorar constructores

--- poo_taller_protocoloindividual/CuentaBancaria.java
+++ poo_taller_protocoloindividual/CuentaBancaria.java
@@ -3,17 +3,19 @@ package repositorio_mjaller.poo_taller_protocoloindividual;
 public class CuentaBancaria {
     //Propiedades
     private String numeroCuenta; //Solo contiene numeros pero no es operable
-    private int saldo;
+    private double saldo;
     private String tipoCuenta;
 
     //1. Constructor por defecto
+
     public CuentaBancaria() {
         this.numeroCuenta = "123456789";
-        this.saldo = 10000000;
+        this.saldo = 0.0;
         this.tipoCuenta = "Ahorros";
     }
 
-    //2. Constructor parametrizado
+    //2. Constructor parametrizado con 2 parametros
+
     public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
         //Validaciones
         if (numeroCuenta == null || numeroCuenta.trim().isEmpty()) { //Se comprueba si la variable es nula o vacia
@@ -21,6 +23,28 @@ public class CuentaBancaria {
             this.numeroCuenta = "000000000";
         } else {
             this.numeroCuenta = numeroCuenta;
+        this.saldo = 0.0; //El saldo se inicializa en 0
+        }
+        if (tipoCuenta == null || tipoCuenta.trim().isEmpty()) { //Se comprueba si la variable es nula o vacia
+            System.out.println("Tipo de cuenta no especificado, se asignará 'Desconocido'\n");
+            this.tipoCuenta = "Desconocido";
+        } else {
+            this.tipoCuenta = tipoCuenta;
+        }
+    }
+
+    //3. Constructor sobrecargado con 3 parametros
+
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
+
+        this.numeroCuenta = numeroCuenta;
+        //Validaciones
+         
+        if (saldo < 0) { //Se comprueba si el saldo es negativo
+            System.out.println("Saldo no válido, se asignará 0 por defecto\n");
+            this.saldo = 0;
+        } else {
+            this.saldo = saldo;
         }
         if (tipoCuenta == null || tipoCuenta.trim().isEmpty()) { //Se comprueba si la variable es nula o vacia
             System.out.println("Tipo de cuenta no especificado, se asignará 'Desconocido'\n");
