e1fcbb5faa0a8a1341ac7ad65717059274def94e
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 12:19:59 -0500

    feat(moto): Mejoras en metodos

--- poo_taller4_protocolocolaborativo/Moto.java
+++ poo_taller4_protocolocolaborativo/Moto.java
@@ -11,15 +11,21 @@ public class Moto {
         return this.marca;
     }
     public void setMarca(String marca){
-        this.marca = marca;
+        if (marca==null || marca.trim().isEmpty()){
+            System.out.println("Marca no especificada o inválida, se le asiganará 'Desconocida' por defecto");
+            this.marca = "Desconocida";
+        }
+        else {this.marca = marca;
+        }
+        
     }
 
-    public void mostrarDetalles() {
+    public void mostrarMoto() {
         System.out.println("""
                 
                 Detalles de la Moto:
                 Marca: %s
-                
+
                 """.formatted(getMarca()));
     }
 }
