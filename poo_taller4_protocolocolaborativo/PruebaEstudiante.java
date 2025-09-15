e91508026669d900070f21f8adc406c2766d9866
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 10:49:51 -0500

    feat(pruebaestudiante): Crear Clase PruebaEstudiante con instancias y acceso a propiedades

---
+++ poo_taller4_protocolocolaborativo/PruebaEstudiante.java
@@ -0,0 +1,23 @@
+package repositorio_mjaller.poo_taller4_protocolocolaborativo;
+
+public class PruebaEstudiante {
+    public static void main(String[] args) {
+        
+        Estudiante e = new Estudiante("Miguel Jaller",26,4.6f);
+        Estudiante e2 = new Estudiante("Anger Piñeres", 18, 3.5f);
+
+        System.out.println("Datos de los estudiantes en 2024: ");
+        e.mostrarNota();
+        e2.mostrarNota();
+        e.setEdad(27);
+        e2.setEdad(19);
+        System.out.println("Datos actualidados en 2024: ");
+        e.mostrarNota();
+        e2.mostrarNota();
+        e.setNotaPromedio(3.8f);
+        e.setNotaPromedio(4.8f);
+        System.out.println("Nota Actualizada hasta 2do Corte de 2025: ");
+        e.mostrarNota();
+
+    }
+}
