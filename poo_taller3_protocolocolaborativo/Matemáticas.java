dd7e5b054c6a2f6beefc90d21dbacbbeffbd9825
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-14 22:36:48 -0500

    feat(matematicas): Crear clase Matematicas con metodos Static

---
+++ poo_taller3_protocolocolaborativo/Matemáticas.java
@@ -0,0 +1,17 @@
+package repositorio_mjaller.poo_taller3_protocolocolaborativo;
+
+public class Matemáticas {
+
+    static int sumar(int a, int b){
+        return a + b;
+    }
+    static int restar(int a, int b){
+        return a - b;
+    }
+    static int multiplicar(int a, int b){
+        return a * b;
+    }
+    static double dividir(double a, double b){
+        return a / b;
+    }    
+}
