aa22bd0df60484c0fb526faef242eeaf7744d706
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 10:08:01 -0500

    feat(estudiante): Crear Clase Estudiante con propiedades privadas

---
+++ poo_taller4_protocolocolaborativo/Estudiante.java
@@ -0,0 +1,14 @@
+package repositorio_mjaller.poo_taller4_protocolocolaborativo;
+
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private float notaPromedio;
+
+    public Estudiante(String nombre, int edad, float notaPromedio){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.notaPromedio = notaPromedio;
+    }
+
+}
