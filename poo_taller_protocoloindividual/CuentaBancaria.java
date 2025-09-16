567c5503e142fe2c1d6cb2729f70481b8d5ac9b1
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 20:53:45 -0500

    feat(cuentabancaria): Taller 1 - implementar buenas prácticas

--- poo_taller_protocoloindividual/CuentaBancaria.java
+++ poo_taller_protocoloindividual/CuentaBancaria.java
@@ -1,12 +1,9 @@
 package repositorio_mjaller.poo_taller_protocoloindividual;
 
 public class CuentaBancaria {
-    //Propiedades
-    private String numeroCuenta; //Solo contiene numeros pero no es operable
+    private String numeroCuenta;
     private double saldo;
     private String tipoCuenta;
-
-    //1. Constructor por defecto
 
     public CuentaBancaria() {
         this.numeroCuenta = "123456789";
@@ -14,38 +11,45 @@ public class CuentaBancaria {
         this.tipoCuenta = "Ahorros";
     }
 
-    //2. Constructor parametrizado con 2 parametros
-
     public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
-        //Validaciones
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = 0.0;
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+        public double getSaldo() {
+        return this.saldo;
+    }
+    public String getTipoCuenta() {
+        return this.tipoCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
         if (numeroCuenta == null || numeroCuenta.trim().isEmpty()) { //Se comprueba si la variable es nula o vacia
             System.out.println("Número de cuenta no especificado, se asignará '000000000'\n");
             this.numeroCuenta = "000000000";
         } else {
             this.numeroCuenta = numeroCuenta;
         }
-        this.saldo = 0.0;
-        if (tipoCuenta == null || tipoCuenta.trim().isEmpty()) { //Se comprueba si la variable es nula o vacia
-            System.out.println("Tipo de cuenta no especificado, se asignará 'Desconocido'\n");
-            this.tipoCuenta = "Desconocido";
-        } else {
-            this.tipoCuenta = tipoCuenta;
-        }
     }
-
-    //3. Constructor sobrecargado con 3 parametros
-
-    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
-
-        this.numeroCuenta = numeroCuenta;
-        //Validaciones
-         
+    public void setSaldo(double saldo) {
         if (saldo < 0.0) { //Se comprueba si el saldo es negativo
             System.out.println("Saldo no válido, se asignará 0 por defecto\n");
             this.saldo = 0.0;
         } else {
             this.saldo = saldo;
         }
+    }
+    public void setTipoCuenta(String tipoCuenta) {
         if (tipoCuenta == null || tipoCuenta.trim().isEmpty()) { //Se comprueba si la variable es nula o vacia
             System.out.println("Tipo de cuenta no especificado, se asignará 'Desconocido'\n");
             this.tipoCuenta = "Desconocido";
@@ -54,29 +58,6 @@ public class CuentaBancaria {
         }
     }
 
-    //4. Metodos get y set
-
-    public String getNumeroCuenta() {
-        return numeroCuenta;
-    }
-    public void setNumeroCuenta(String numeroCuenta) {
-        this.numeroCuenta = numeroCuenta;
-    }
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-    public String getTipoCuenta() {
-        return tipoCuenta;
-    }
-    public void setTipoCuenta(String tipoCuenta) {
-        this.tipoCuenta = tipoCuenta;
-    }
-
-    //Metodos mostrarDatos
     @Override
     public String toString() {
         return
