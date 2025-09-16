2f8c8b49c568a6287a02b11e110e5285646dcdb3
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-16 01:11:13 -0500

    feat(empleado): Crear Clase Empleado con atributis protected

---
+++ poo_actividad_unidad1/Empleado.java
@@ -0,0 +1,16 @@
+package repositorio_mjaller.poo_actividad_unidad1;
+
+class Empleado {
+  
+    protected String nombre;
+    protected double salario;
+
+    Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    protected void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre + ", Salario: " + salario);
+    }
+}
