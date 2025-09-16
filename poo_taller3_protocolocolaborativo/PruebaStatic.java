9d381a8b3a3a1c71a783344788407206c30543f6
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 21:45:39 -0500

    feat(pruebastatic): Corregir código

--- poo_taller3_protocolocolaborativo/PruebaStatic.java
+++ poo_taller3_protocolocolaborativo/PruebaStatic.java
@@ -1,25 +1,23 @@
 package repositorio_mjaller.poo_taller3_protocolocolaborativo;
 
 public class PruebaStatic {
-    private int contador; 
+    //private int contador; //Error: non-static variable this cannot be referenced from a static context
+    static int contador = 0;
 
     public static void incrementar() {
         contador++;
+        System.out.println("\nContador: " + contador);
     }
 
     public static void main(String[] args) {
         incrementar();
+        
     }
+}
     
-    //Error: non-static variable this cannot be referenced from a static context
     //Este error indica que debido a que la variable no es estática no puede ser referenciada en un contexto Estático
     //Los metódos estáticos solo pueden accedér a variables estáticas
 
-    //Solución 1:
+    //Solución:
     //Para que la variable pueda ser referenciada debemos convertir la variable a Static
 
-    //Solucion 2:
-    //El metodo para mostrar las variable que NO son estáticas tampoco debe ser un método de instancia
-    
-
-}
