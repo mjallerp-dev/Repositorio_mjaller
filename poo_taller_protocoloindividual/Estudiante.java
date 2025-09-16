4eff29906b20fbee09d1c4ba1231a29598d17207
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 20:45:14 -0500

    feat(estudiante): Taller 1 - Implementar buenas prácticas

--- poo_taller_protocoloindividual/Estudiante.java
+++ poo_taller_protocoloindividual/Estudiante.java
@@ -1,40 +1,55 @@
 package repositorio_mjaller.poo_taller_protocoloindividual;
 
 public class Estudiante {
-    //Propiedades
     private String nombre;
     private int edad;
     private int curso;
 
     public static int contador = 0;
 
-    //1. Constructor por defecto
     public Estudiante() {
         this.nombre = "Juan Pérez";
         this.edad = 20;
         this.curso = 10;
     }
 
-    //2. Constructor parametrizado con 2 parametros (nombre, edad)
     public Estudiante(String nombre, int edad) {
-        if (nombre == null || nombre.trim().isEmpty()) { //Se comprueba si la variable es nula o vacia
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = 1; // 
+    }
+
+    public Estudiante(String nombre, int edad, int curso) {
+        this(nombre, edad);
+        this.curso = curso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+        public int getEdad() {
+        return edad;
+    }
+        public int getCurso() {
+        return curso;
+    }
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.trim().isEmpty()) {
             System.out.println("Nombre no especificado, se asignará 'Nombre Desconocido'\n");
             this.nombre = "Nombre Desconocido";
         } else {
             this.nombre = nombre;
         }
+    }
+    public void setEdad(int edad) {
         if (edad <= 1) {
             System.out.println("Edad no válida, se asignará '1' por defecto\n");
             this.edad = 1;
         } else {
             this.edad = edad;
         }
-        this.curso = 1; // Curso por defecto
     }
-
-    //3. Constructor con todos los parametros y uso de this()
-    public Estudiante(String nombre, int edad, int curso) {
-        this(nombre, edad); // Llama al constructor anterior
+    public void setCurso(int curso) {
         if (curso < 0 || curso > 11) {
             System.out.println("Curso no válido, se asignará '1' por defecto\n");
             this.curso = 1;
@@ -42,28 +57,6 @@ public class Estudiante {
             this.curso = curso;
         }
     }
-
-    //4. Metodos get y set
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public int getEdad() {
-        return edad;
-    }
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    public int getCurso() {
-        return curso;
-    }
-    public void setCurso(int curso) {
-        this.curso = curso;
-    }
-
-    //Metodos para mostrar datos
 
     @Override
     public String toString() {
