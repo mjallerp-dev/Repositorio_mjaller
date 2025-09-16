e4ea3bf599873cdc1a907cffb4f2a968006b9975
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 23:57:43 -0500

    feat(producto): Crear clase Producto

---
+++ poo_actividad_unidad1/Producto.java
@@ -0,0 +1,22 @@
+package repositorio_mjaller.poo_actividad_unidad1;
+
+public class Producto {
+    String nombre;   
+    double precio;   
+    int stock;       
+
+    Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    // Método default
+    void mostrarInfo() {
+        System.out.println("Nombre: " + nombre + 
+                           ", Precio: " + precio + 
+                           ", Stock: " + stock);
+    }
+}
+
+
