c336890d679f75b9e09dd35099d49938c5c0d2a5
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-16 00:37:37 -0500

    feat(persona): Crear clase persona con metodos get y set

---
+++ poo_actividad_unidad1/Persona.java
@@ -0,0 +1,25 @@
+package repositorio_mjaller.poo_actividad_unidad1;
+
+class Persona {
+    private String nombre;
+    int edad;
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+    
+
+
+
+}
