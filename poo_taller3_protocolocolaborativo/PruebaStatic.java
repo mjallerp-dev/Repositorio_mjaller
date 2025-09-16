e7d1be8211a56124c1ad8668265835faa649401f
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 21:26:30 -0500

    feat(pruebastatic): Limpiar código de metodo static

--- poo_taller3_protocolocolaborativo/PruebaStatic.java
+++ poo_taller3_protocolocolaborativo/PruebaStatic.java
@@ -1,46 +1,14 @@
 package repositorio_mjaller.poo_taller3_protocolocolaborativo;
 
 public class PruebaStatic {
-    private static String nombre = "Miguel";
-    private static int edad = 26;
-    private static String programa = "Ing. de Software";
-    static int contador = 0;
+    private int contador; 
 
-    //public PruebaStatic(String nombre, int edad, String programa) {
-    //    this.nombre = nombre;
-    //    this.edad = edad;
-    //    this.programa = programa;
-    //}
-
-    //public PruebaStatic(String nombre,int edad, String programa){
-    //    this.nombre = nombre;
-    //    this.edad = edad;
-    //    this.programa = programa;
-    //}
-
-    //public String getNombre(){
-    //    return this.nombre;
-    //}
-    //public int getEdad() {
-    //    return this.edad;
-    //}
-    //public String getPrograma(){
-    //    return this.programa;
-    //}
-    
-    public static void mostrarDatos() {
+    public static void incrementar() {
         contador++;
-        System.out.println("\nDatos del Estudiante " + contador);
-        System.out.println("\nNombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Programa: " + programa);
     }
 
     public static void main(String[] args) {
-
-        //Estudiante estudiante = new Estudiante("Jose",24,"Ing. Civil");
-        //estudiante.mostrarDatos();
-        mostrarDatos();
+        incrementar();
     }
     
     //Error: non-static variable this cannot be referenced from a static context
