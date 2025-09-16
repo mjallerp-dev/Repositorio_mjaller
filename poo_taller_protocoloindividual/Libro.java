278752456e46e50f01dade0639887cd68be49ea0
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 20:49:13 -0500

    feat(libro): Taller 1 - Implementar buenas prácticas

--- poo_taller_protocoloindividual/Libro.java
+++ poo_taller_protocoloindividual/Libro.java
@@ -1,55 +1,21 @@
 package repositorio_mjaller.poo_taller_protocoloindividual;
 
 public class Libro{
-    //En los atributos aplico el encapsulamiento poniendolos como privados
     private String titulo;
     private String autor;
     private int numeroPaginas;
 
-    //Este es un contador estatico para llevar la cuenta de los libros creados
-
-    //1. Constructor por defecto
     public Libro() {
         this.titulo = "Cien años de soledad";
         this.autor = "Gabriel García Márquez";
         this.numeroPaginas = 471;
     }
 
-    //2. Constructor parametrizado
     public Libro(String titulo,String autor, int numeroPaginas) {
-    
-        //Validaciones: Se comprueba si la variable es nula o vacia
-
-        if (titulo==null || titulo.trim().isEmpty()){ //Se comprueba si la variable es nula o vacia
-            System.out.println("Título no especificado, se asignará 'Título Desconocido'\n");
-            this.titulo="Título Desconocido";
-        }
-        else {this.titulo = titulo;
-        }
-        if (autor==null || autor.trim().isEmpty()){
-            System.out.println("Autor no especificado, se asignará 'Autor Desconocido'\n");
-            this.autor="Autor Desconocido";
-        }
-        else {this.autor = autor;
-        }
-        this.numeroPaginas = numeroPaginas;
-    }
-
-    //3.Metodos
-
-    //Metodo set
-
-    public void setTitulo(String titulo) {
         this.titulo = titulo;
-    }
-    public void setAutor(String autor) {
         this.autor = autor;
-    }
-    public void setNumeroPaginas(int numeroPaginas) {
         this.numeroPaginas = numeroPaginas;
     }
-
-    //Metodo get
 
     public String getTitulo() {
         return this.titulo;
@@ -61,7 +27,29 @@ public class Libro{
         return this.numeroPaginas;
     }
 
-    //Metodo para mostrar la informacion del libro
+    public void setTitulo(String titulo) {
+        if (titulo==null || titulo.trim().isEmpty()){ //Se comprueba si la variable es nula o vacia
+            System.out.println("Título no especificado, se asignará 'Título Desconocido'\n");
+            this.titulo="Título Desconocido";
+        }
+        else {this.titulo = titulo;
+        }
+    }
+    public void setAutor(String autor) {
+        if (autor==null || autor.trim().isEmpty()){
+            System.out.println("Autor no especificado, se asignará 'Autor Desconocido'\n");
+            this.autor="Autor Desconocido";
+        }
+        else {this.autor = autor;
+        }
+    }
+    public void setNumeroPaginas(int numeroPaginas) {
+        if (numeroPaginas<1){
+            System.out.println("Numero de paginas no especificado o inválido, se asignará 'Paginas desconocidad'\n");
+        }
+        else {this.numeroPaginas = numeroPaginas;
+        }
+    }
 
     @Override
     public String toString() {
