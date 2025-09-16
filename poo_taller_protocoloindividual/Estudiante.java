edcbf3483bdec425c63605a57ed841c8aac2a245
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-15 22:59:00 -0500

    feat(estudiante): Mejoras en metodos

--- poo_taller_protocoloindividual/Estudiante.java
+++ poo_taller_protocoloindividual/Estudiante.java
@@ -5,8 +5,6 @@ public class Estudiante {
     private int edad;
     private int curso;
 
-    public static int contador = 0;
-
     public Estudiante() {
         this.nombre = "Juan Pérez";
         this.edad = 20;
@@ -25,14 +23,15 @@ public class Estudiante {
     }
 
     public String getNombre() {
-        return nombre;
+        return this.nombre;
     }
-        public int getEdad() {
-        return edad;
+    public int getEdad() {
+        return this.edad;
     }
-        public int getCurso() {
-        return curso;
+    public int getCurso() {
+        return this.curso;
     }
+
     public void setNombre(String nombre) {
         if (nombre == null || nombre.trim().isEmpty()) {
             System.out.println("Nombre no especificado, se asignará 'Nombre Desconocido'\n");
@@ -41,14 +40,16 @@ public class Estudiante {
             this.nombre = nombre;
         }
     }
+
     public void setEdad(int edad) {
-        if (edad <= 1) {
+        if (edad < 1) {
             System.out.println("Edad no válida, se asignará '1' por defecto\n");
             this.edad = 1;
         } else {
             this.edad = edad;
         }
     }
+
     public void setCurso(int curso) {
         if (curso < 0 || curso > 11) {
             System.out.println("Curso no válido, se asignará '1' por defecto\n");
@@ -60,7 +61,6 @@ public class Estudiante {
 
     @Override
     public String toString() {
-        contador+=1;
         return
             "Nombre: " + getNombre() + "\n" +
             "Edad: " + getEdad() + "\n" +
