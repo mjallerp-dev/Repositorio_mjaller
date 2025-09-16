fef3648a12a93f3f1735ea1290163e8d990d7cba
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-16 00:51:52 -0500

    feat(persona): Modificar  metodos get y set

--- poo_actividad_unidad1/Persona.java
+++ poo_actividad_unidad1/Persona.java
@@ -7,19 +7,9 @@ class Persona {
     public String getNombre() {
         return this.nombre;
     }
-    public int getEdad() {
-        return this.edad;
-    }
+    
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-
-    
-
-
 
 }
