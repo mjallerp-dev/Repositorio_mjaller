22632a10dc249ad53783db14fd2cc7146ef462f8
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-30 00:23:02 -0500

    feat(empleado): Agregar metodos get y set

--- unidad2_poo/taller8/Empleado.java
+++ unidad2_poo/taller8/Empleado.java
@@ -9,9 +9,24 @@ public class Empleado {
         this.edad = edad;
     }
 
+    public String getNombre() {
+        return this.nombre;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre== null || nombre.trim().isEmpty()){
+            System.out.println("El nombre no puede estar vacío, se asigna 'Desconocido' por defecto");
+            this.nombre = "Desconocido";
+        }
+        else {this.nombre = nombre;}
+    }
+
     public void mostrarDetalles() {
         System.out.println("Datos del Empleado: ");
-        System.out.println("Nombre: " + this.nombre);
-        System.out.println("Edad: " + this.edad);
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Edad: " + getEdad());
     }
 }
