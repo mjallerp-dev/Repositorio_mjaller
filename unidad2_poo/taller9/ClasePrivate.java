2b047df4fba7f621b6e8d2fb57d1d0c551e5879b
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-30 17:53:29 -0500

    feat(claseprivate): Crear clase con atributo privado

---
+++ unidad2_poo/taller9/ClasePrivate.java
@@ -0,0 +1,13 @@
+package repositorio_mjaller.unidad2_poo.taller9;
+
+public class ClasePrivate {
+    private String nombrePrivado;
+
+    public ClasePrivate(String nombrePrivado) {
+        this.nombrePrivado = nombrePrivado;
+    }
+
+    public String getNombrePrivado() {
+        return this.nombrePrivado;
+    }
+}
