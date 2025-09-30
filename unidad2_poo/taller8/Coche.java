4b708ce36893217a4c4bf4692c1b3decaffedd05
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 23:41:39 -0500

    feat(coche): Agregar metodos para mostrar detalles de Vehiculo y Coche

--- unidad2_poo/taller8/Coche.java
+++ unidad2_poo/taller8/Coche.java
@@ -7,4 +7,21 @@ public class Coche extends Vehiculo {
         super(marca, velocidadMaxima);
         this.numeroPuertas = numeroPuertas;
     }
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Numero de Puertas: " + this.numeroPuertas);
+    }
+
+    public static void main(String[] args) {
+        Vehiculo vehiculo1 = new Vehiculo("Toyota", 180);
+        vehiculo1.mostrarDetalles();
+        Vehiculo vehiculo2 = new Vehiculo("Ford", 220);
+        vehiculo2.mostrarDetalles();
+        Coche coche1 = new Coche("Ferrari", 300, 2);
+        coche1.mostrarDetalles();
+        Coche coche2 = new Coche("Honda", 200, 4);
+        coche2.mostrarDetalles();
+    }
 }
