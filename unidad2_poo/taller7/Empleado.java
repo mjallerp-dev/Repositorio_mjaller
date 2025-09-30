c1201af15d69365d20781e35a7a68be1d5c482e3
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 22:16:44 -0500

    feat(empleado): Constructor y metodos get y set publicos, con validaciones en metodo set

--- unidad2_poo/taller7/Empleado.java
+++ unidad2_poo/taller7/Empleado.java
@@ -4,4 +4,19 @@ public class Empleado {
     public String nombre;
     int salario;
 
+    public Empleado(String nombre, int salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public int getNombre() {
+        return this.salario;
+    }
+    public void setSalario(int salario) {
+        if (salario<0) { 
+            System.out.println("El salario no puede ser negativo, se asignara 0 por defecto");
+            this.salario=0;
+        }
+        else {this.salario = salario;}
+    }
 }
