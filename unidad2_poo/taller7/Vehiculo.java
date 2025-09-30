9db58a99eea1ff2359cad3a5c5aec3b8578e1e30
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 23:11:44 -0500

    feat(vehiculo): Taller 8 Ejercicio 1 Crear clase vehiculo

---
+++ unidad2_poo/taller7/Vehiculo.java
@@ -0,0 +1,12 @@
+package repositorio_mjaller.unidad2_poo.taller7;
+
+public class Vehiculo {
+    private String marca;
+    private int velocidadMaxima;
+
+    public Vehiculo(String marca, int velocidadMaxima) {
+        this.marca = marca;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+}
