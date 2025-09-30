3e65212de5261431ba92b0576a301b35cf05e41d
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 22:25:45 -0500

    feat(empleado): Actualizar metodos y crear metodo mostrar

--- unidad2_poo/taller7/Empleado.java
+++ unidad2_poo/taller7/Empleado.java
@@ -1,4 +1,4 @@
-package unidad2_poo.taller7;
+package repositorio_mjaller.unidad2_poo.taller7;
 
 public class Empleado {
     public String nombre;
@@ -12,11 +12,26 @@ public class Empleado {
     public int getNombre() {
         return this.salario;
     }
+    public int getSalario() {
+        return this.salario;
+    }
+    public void setNombre(String nombre) {
+        if (nombre==null || nombre.trim().isEmpty()) {
+            System.out.println("El nombre no puede estar vacio, se asignara 'Desconocido' por defecto");
+            this.nombre="Desconocido";
+        }
+        else
+        this.nombre = nombre;
+    }
     public void setSalario(int salario) {
         if (salario<0) { 
             System.out.println("El salario no puede ser negativo, se asignara 0 por defecto");
             this.salario=0;
         }
         else {this.salario = salario;}
+    }
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Salario: " + getSalario());
     }
 }
