774d7bc2cd4dc3f11c6a0b3a6d618c73cd5e7382
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 22:37:41 -0500

    feat(cuentabancaria): Crear Clase CuentaBancaria con propiedades y constructor

---
+++ unidad2_poo/taller7/CuentaBancaria.java
@@ -0,0 +1,13 @@
+package repositorio_mjaller.unidad2_poo.taller7;
+
+public class CuentaBancaria {
+    private String numeroCuenta;
+    private double saldo;
+    public String tipoCuenta;
+
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoCuenta = tipoCuenta;
+    }
+}
