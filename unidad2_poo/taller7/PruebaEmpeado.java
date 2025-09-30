a76e9a4025151cf18f3c40e8d4a3902441d743d8
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 22:32:58 -0500

    feat(pruebaempleado): Crear Clase prueba que intancia objetos de Empleado

---
+++ unidad2_poo/taller7/PruebaEmpeado.java
@@ -0,0 +1,12 @@
+package repositorio_mjaller.unidad2_poo.taller7;
+
+public class PruebaEmpeado {
+
+    public static void main(String[] args) {
+        Empleado emp1 = new Empleado("Miguel", 1000);
+        Empleado emp2 = new Empleado("Angel", 500);
+        emp1.mostrarDatos();
+        emp2.mostrarDatos();
+    }
+
+}
