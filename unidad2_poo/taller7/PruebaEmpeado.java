c687cc900fe451810891d7a1cb71ce60490cc651
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 22:34:35 -0500

    feat(pruebaempleado): Agregar uso de metodos get y set

--- unidad2_poo/taller7/PruebaEmpeado.java
+++ unidad2_poo/taller7/PruebaEmpeado.java
@@ -7,6 +7,12 @@ public class PruebaEmpeado {
         Empleado emp2 = new Empleado("Angel", 500);
         emp1.mostrarDatos();
         emp2.mostrarDatos();
+        emp1.setNombre(null);
+        emp2.setNombre("  ");
+        emp1.setSalario(-200);
+        emp2.setSalario(1500);
+        emp1.mostrarDatos();
+        emp2.mostrarDatos();
     }
 
 }
