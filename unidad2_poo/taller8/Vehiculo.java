b8e79bd568271f48d6331e31b407b4f4a8a7d508
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 23:21:51 -0500

    feat(vehiculo): Modificar acceso de propiedades

--- unidad2_poo/taller8/Vehiculo.java
+++ unidad2_poo/taller8/Vehiculo.java
@@ -1,8 +1,8 @@
 package repositorio_mjaller.unidad2_poo.taller8;
 
 public class Vehiculo {
-    private String marca;
-    private int velocidadMaxima;
+    protected String marca;
+    protected int velocidadMaxima;
 
     public Vehiculo(String marca, int velocidadMaxima) {
         this.marca = marca;
