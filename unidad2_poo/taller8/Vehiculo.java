8d0c920947909b0ea5769865ecdb51b2832f2e5e
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-29 23:12:53 -0500

    feat(vahiculo): Mover clase vehiculo

---
+++ unidad2_poo/taller8/Vehiculo.java
@@ -0,0 +1,11 @@
+package repositorio_mjaller.unidad2_poo.taller8;
+
+public class Vehiculo {
+    private String marca;
+    private int velocidadMaxima;
+
+    public Vehiculo(String marca, int velocidadMaxima) {
+        this.marca = marca;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+}
