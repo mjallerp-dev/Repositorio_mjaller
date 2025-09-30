8ce8ea07dca9709ca572690117f36436408d252f
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-30 00:24:58 -0500

    feat(gerente): Agregar metodos get y set

--- unidad2_poo/taller8/Gerente.java
+++ unidad2_poo/taller8/Gerente.java
@@ -7,4 +7,20 @@ public class Gerente extends Empleado {
         super(nombre, edad);
         this.departamento = departamento;
     }
+
+    public String getDepartamento() {
+        return this.departamento;
+    }
+    public void setDepartamento(String departamento) {
+        if (departamento == null || departamento.trim().isEmpty()) {
+            System.out.println("El departamento no puede estar vacío, se asigna 'Desconocido' por defecto");
+            this.departamento = "Desconocido";
+        } else {this.departamento = departamento;}
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Departamento: " + getDepartamento());
+    }
 }
