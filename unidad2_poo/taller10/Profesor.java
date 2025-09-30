101eaa887337ab26b45a18ee97858b401a11e785
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-30 18:14:26 -0500

    feat(profesor): Crear Clase profesor que herede de Persona

---
+++ unidad2_poo/taller10/Profesor.java
@@ -0,0 +1,10 @@
+package repositorio_mjaller.unidad2_poo.taller10;
+
+public class Profesor extends Persona {
+    private String asignatura;
+
+    public Profesor(String nombre, int edad, String asignatura) {
+        super(nombre, edad);
+        this.asignatura = asignatura;
+    }
+}
