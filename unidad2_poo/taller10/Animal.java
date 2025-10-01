3baa64659fd4a84af110aec008dd63eb95b3fe52
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-09-30 20:22:28 -0500

    feat(animal): Taller 10 Ejercicio 3 Crear clase padre

---
+++ unidad2_poo/taller10/Animal.java
@@ -0,0 +1,17 @@
+package repositorio_mjaller.unidad2_poo.taller10;
+
+public class Animal {
+    protected String especie;
+
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+
+    public String getEspecie() {
+        return this.especie;
+    }
+
+    public void sonido() {
+        System.out.println("El" + getEspecie() + "hace un sonido.");
+    }
+}
