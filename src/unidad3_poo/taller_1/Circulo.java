ba014272e6bdf8239c1f2bccb889ef5dc981322d
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-18 18:24:08 -0500

    feat(Circulo): Crear clase Circulo que hereda de Figura e implementar metido abstracto

---
+++ src/unidad3_poo/taller_1/Circulo.java
@@ -0,0 +1,10 @@
+package unidad3_poo.taller_11;
+
+public class Circulo extends Figura {
+
+    @Override
+    public void calcularArea() {
+        System.out.println("El área del círculo se calcula con la fórmula: A = π * radio^2");
+    }
+
+}
--- src/unidad3_poo/taller_11/Figura.java
+++
@@ -1,5 +0,0 @@
-package unidad3_poo.taller_11;
-
-public abstract class Figura {
-    public abstract void calcularArea();
-}
