eafd862eed1e9b52779f32b3f048050cf4c13362
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-18 19:44:45 -0500

    feat(Rectangulo): Añadir metodos get y set con validaciones

--- src/unidad3_poo/taller_11/ejercicio_1/Rectangulo.java
+++ src/unidad3_poo/taller_11/ejercicio_1/Rectangulo.java
@@ -9,6 +9,23 @@ public class Rectangulo extends Figura {
         this.altura = altura;
     }
 
+    public double getBase() {
+        return this.base;
+    }
+    public double getAltura() {
+        return this.altura;
+    }
+    public void setBase(double base) {
+        if (base <= 0) {
+            System.out.println("El valor de la base es inválido");
+        }else{this.base = base;}
+    }
+    public void setAltura(double altura) {
+        if (altura <= 0) {
+            System.out.println("El valor de la altura es inválido");
+        }else{this.altura = altura;}
+    }
+
     @Override
     public double calcularArea() {
         return (this.base * this.altura);
