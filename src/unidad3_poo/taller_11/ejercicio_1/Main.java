c1a056ec7eeb35d2d11681825435fb3ad7dcc0c4
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-18 20:14:49 -0500

    feat(Main): Añadir metodos set para mostrar validaciones

--- src/unidad3_poo/taller_11/ejercicio_1/Main.java
+++ src/unidad3_poo/taller_11/ejercicio_1/Main.java
@@ -6,8 +6,19 @@ public class Main {
         Circulo circulo = new Circulo(3);
         Rectangulo rectangulo = new Rectangulo(4, 5);
 
+        System.out.println("\nÁrea del circulo: ");
         circulo.mostrarArea();
+        System.out.println("\nÁrea del circulo: ");
         rectangulo.mostrarArea();
+        System.out.println("\n_________________________________");
+        circulo.setRadio(-2);
+        rectangulo.setBase(0);
+        rectangulo.setAltura(7);
+        System.out.println("\nÁrea del circulo: ");
+        circulo.mostrarArea();
+        System.out.println("\nÁrea del rectángulo: ");
+        rectangulo.mostrarArea();
+
     }
 
 }
