25348960432f9b642f488ef0cee30ee427a06fda
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-19 10:35:17 -0500

    feat(AnimalHerbivoro): Crear clase AnimalHerviboro y sobreescribir metodo de la clase abstracta

---
+++ src/unidad3_poo/taller_11/ejercicio_3/AnimalHerbivoro.java
@@ -0,0 +1,10 @@
+package taller_11.ejercicio_3;
+
+public class AnimalHerbivoro extends Animal {
+
+    @Override
+    public void Alimentarse() {
+        System.out.println("El animal Herbivoro se alimenta de Hierba");
+    }
+    
+}
