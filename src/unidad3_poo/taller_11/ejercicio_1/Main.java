09d7fad48b94a19142fbebce4c93ca193d8cf744
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-18 19:50:55 -0500

    feat(Main): Crear metodo Main para instanciar los objetos y mostrar metodo mostrarArea

---
+++ src/unidad3_poo/taller_11/ejercicio_1/Main.java
@@ -0,0 +1,13 @@
+package taller_11.ejercicio_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo(3);
+        Rectangulo rectangulo = new Rectangulo(4, 5);
+
+        circulo.mostrarArea();
+        rectangulo.mostrarArea();
+    }
+
+}
