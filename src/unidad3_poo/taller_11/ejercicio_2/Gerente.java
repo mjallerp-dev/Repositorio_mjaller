a2e37ae625a910ce85dc3e7c8a41568ba085a799
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-18 20:59:07 -0500

    feat(Gerente): Añadir metodos getter - setter y reordenar codigo para mejorar lógica

--- src/unidad3_poo/taller_11/ejercicio_2/Gerente.java
+++ src/unidad3_poo/taller_11/ejercicio_2/Gerente.java
@@ -2,18 +2,37 @@ package taller_11.ejercicio_2;
 
 public class Gerente extends Empleado {
     private double salarioDiario;
-    private double diasTrabajados;
-    private double bono;
+    private int diasTrabajados;
+    double bono = 1000000;
 
-    public Gerente(double salarioDiario, double diasTrabajados, double bono) {
+    public Gerente(double salarioDiario, int diasTrabajados) {
         this.salarioDiario = salarioDiario;
         this.diasTrabajados = diasTrabajados;
-        this.bono = bono;
+    }
+
+    public double getSalarioDiario() {
+        return this.salarioDiario;
+    }
+    public int getDiasTrabajados() {
+        return this.diasTrabajados;
+    }
+
+    public void setSalarioDiario(double salarioDiario) {
+        if (salarioDiario < 47450) {
+            System.out.println("El salario diario minimo diario es de 47450, se asignará este valor por defecto");
+            this.salarioDiario = 47450;
+        }else{this.salarioDiario = salarioDiario;}
+    }
+    public void setDiasTrabajados(int diasTrabajados) {
+        if (diasTrabajados < 0) {
+            System.out.println("Los días trabajados no pueden ser negativos, se asignará 1 por defecto");
+            this.diasTrabajados = 1;
+        }else{this.diasTrabajados = diasTrabajados;}
     }
 
     @Override
     public double calcularSalario() {
-        return (this.salarioDiario * this.diasTrabajados) + this.bono;
+        return ((getSalarioDiario() * getDiasTrabajados()) + bono);
     }
 
 }
