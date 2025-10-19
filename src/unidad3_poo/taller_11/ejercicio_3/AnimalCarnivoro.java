13c3b343997c64f7cc0b371b2ded90f9b14ce517
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-19 10:32:33 -0500

    feat(AnimalCarnivoro): Crear clase AnimalCarnivoro y sobreescribir metodo de la clase abstracta

---
+++ src/unidad3_poo/taller_11/ejercicio_3/AnimalCarnivoro.java
@@ -0,0 +1,10 @@
+package taller_11.ejercicio_3;
+
+public class AnimalCarnivoro extends Animal {
+    
+    @Override
+    public void Alimentarse() {
+        System.out.println("El animal Carnivoro se alimenta de Carne");
+    }
+
+}
