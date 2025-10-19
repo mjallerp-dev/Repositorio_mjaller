9d3b9d050d2a3fc8725f9ad8da87816fabb42b01
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-19 11:03:18 -0500

    feat(Main): Crear clase Main con metodo para instanciar objetos directamente de la clase Abstracta

---
+++ src/unidad3_poo/taller_11/ejercicio_3/Main.java
@@ -0,0 +1,13 @@
+package taller_11.ejercicio_3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Animal animal = new Animal();
+
+        animal.Alimentarse();
+
+    }
+
+}
