81c48e6737a8c19881275fa64e8cbd80ac75e468
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-18 21:09:38 -0500

    feat(Vendedor): Añadir metodos getter y setter

--- src/unidad3_poo/taller_11/ejercicio_2/Vendedor.java
+++ src/unidad3_poo/taller_11/ejercicio_2/Vendedor.java
@@ -12,9 +12,38 @@ public class Vendedor extends Empleado {
         this.diasTrabajados = diasTrabajados;
     }
 
+    public double getSalarioDiario() {
+        return this.salarioDiario;
+    }
+    public int getNumeroVentas() {
+        return this.numeroVentas;
+    }
+    public int getDiasTrabajados() {
+        return this.diasTrabajados;
+    }
+
+    public void setSalarioDiario(double salarioDiario) {
+        if (salarioDiario < 47450) {
+            System.out.println("El salario diario minimo diario es de 47450, se asignará este valor por defecto");
+            this.salarioDiario = 47450;
+        }else{this.salarioDiario = salarioDiario;}
+    }
+    public void setNumeroVentas(int numeroVentas) {
+        if (numeroVentas < 0) {
+            System.out.println("El número de ventas no puede ser negativo, se asignará 0 por defecto");
+            this.numeroVentas = 0;
+        }else{this.numeroVentas = numeroVentas;}
+    }
+    public void setDiasTrabajados(int diasTrabajados) {
+        if (diasTrabajados < 0) {
+            System.out.println("Los días trabajados no pueden ser negativos, se asignará 1 por defecto");
+            this.diasTrabajados = 1;
+        }else{this.diasTrabajados = diasTrabajados;}
+    }
+
     @Override
     public double calcularSalario() {
-        return (this.salarioDiario * this.diasTrabajados) + (this.numeroVentas * this.comisionPorVenta);
+        return ((getSalarioDiario() * getDiasTrabajados()) + (getNumeroVentas() * comisionPorVenta));
     }
 
 }
