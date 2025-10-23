65aab977ab385e9e04ebe0bbb68dece6cfd73a2b
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-22 23:14:01 -0500

    feat(Main): Crear clase Main para instanciar objetos de la clase Pez y usar metodos de las interfaces

---
+++ src/unidad3_poo/taller_12/ejercicio_2/Main.java
@@ -0,0 +1,13 @@
+package taller_12.ejercicio_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Pez pez = new Pez();
+
+        pez.nadar();
+        pez.respirar();
+    }
+    
+}
