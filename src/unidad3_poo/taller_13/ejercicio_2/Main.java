1511152afd88a57ceae25fc0908ffd07bfe6b705
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-30 23:05:00 -0500

    feat(Main): crear clase Main para instanciar objeto Pato y los metodos de las interfaces

---
+++ src/unidad3_poo/taller_13/ejercicio_2/Main.java
@@ -0,0 +1,11 @@
+package taller_13.ejercicio_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pato pato = new Pato();
+        pato.nadar();
+        pato.volar();
+    }
+}
