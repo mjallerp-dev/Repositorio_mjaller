f7692ab32a739cd9b95cc4a4c63321f9c4602ad8
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-30 22:31:05 -0500

    feat(Rectangulo): Crear metodos get y set para buenas prácticas

--- src/unidad3_poo/taller_13/Rectangulo.java
+++ src/unidad3_poo/taller_13/Rectangulo.java
@@ -9,4 +9,24 @@ public class Rectangulo extends Figura {
         this.altura = altura;
     }
 
+    public double getBase() {
+        return this.base;
+    }
+    public double getAltura() {
+        return this.altura;
+    }
+
+    public void setBase(double base) {
+        if (base <= 0) {
+            System.out.println("El valor de la base es inválido, se asignará 1 por defecto");
+            this.base = 1;
+        }else{this.base = base;}
+    }
+    public void setAltura(double altura) {
+        if (altura <= 0) {
+            System.out.println("El valor de la altura es inválido, se asignará 1 por defecto");
+            this.altura = 1;
+        }else{this.altura = altura;}
+    }
+
 }
