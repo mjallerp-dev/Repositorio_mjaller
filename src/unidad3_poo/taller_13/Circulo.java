2f512e924d46719374f39c7fc99a7c475f661382
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-30 22:38:10 -0500

    feat(Circulo): Crear metodos get y set, y sobreescribir metodo abstracto

--- src/unidad3_poo/taller_13/Circulo.java
+++ src/unidad3_poo/taller_13/Circulo.java
@@ -7,4 +7,20 @@ public class Circulo extends Figura {
         this.radio = radio;
     }
 
+    public double getRadio() {
+    return this.radio;
+    }
+
+    public void setRadio(double radio) {
+        if (radio <= 0) {
+            System.out.println("El valor del radio es inválido, se asignará 1 por defecto");
+            this.radio = 1;
+        }else{this.radio = radio;}
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(getRadio(), 2);
+    }
+
 }
