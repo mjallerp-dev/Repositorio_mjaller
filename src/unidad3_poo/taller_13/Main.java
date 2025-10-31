6c9307eca41394bc56266a8f8a8a992262480b3c
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-10-30 22:53:18 -0500

    feat(Main): Usar metodos set para validacion

--- src/unidad3_poo/taller_13/Main.java
+++ src/unidad3_poo/taller_13/Main.java
@@ -3,10 +3,14 @@ package taller_13;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         Circulo circulo = new Circulo(5);
         Rectangulo rectangulo = new Rectangulo(4, 6);
 
+        System.out.println("El área del círculo es: " + circulo.calcularArea());
+        System.out.println("El área del rectángulo es: " + rectangulo.calcularArea());
+        circulo.setRadio(-3);
+        rectangulo.setBase(0);
         System.out.println("El área del círculo es: " + circulo.calcularArea());
         System.out.println("El área del rectángulo es: " + rectangulo.calcularArea());
     }
