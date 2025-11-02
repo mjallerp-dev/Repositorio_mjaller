4b51db486855ba5d0b54e7e2d184e77ede413e5e
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-01 19:17:50 -0500

    feat(Main): Crear metodo main para demostrar polimorfismo

---
+++ src/unidad3_poo/taller_14/ejercicio_1/Main.java
@@ -0,0 +1,15 @@
+package taller_14.ejercicio_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Figura figura1 = new Rectangulo(5, 10);
+        Figura figura2 = new Circulo(5);
+
+        figura1.calcularArea();
+        figura2.calcularArea();
+        
+    }
+
+}
