87bc551a147ab3ea040066351e916c2e48125006
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-01 19:13:23 -0500

    feat(Circulo): Actualizar metodo set y calcularArea

--- src/unidad3_poo/taller_14/ejercicio_1/Circulo.java
+++ src/unidad3_poo/taller_14/ejercicio_1/Circulo.java
@@ -11,12 +11,16 @@ public class Circulo extends Figura {
         return radio;
     }
     public void setRadio(double radio) {
-        this.radio = radio;
+        if (radio <= 0) {
+            System.out.println("El valor del radio es inválido, se asignará 1 por defecto");
+            this.radio = 1;
+        }
+        else{this.radio = radio;}
     }
 
     @Override
     public void calcularArea() {
-        System.out.println("El area del circulo es: " + Math.PI * Math.pow(radio, 2));
+        System.out.println("El area del circulo es: " + Math.PI * Math.pow(getRadio(), 2));
     }
 
 }
