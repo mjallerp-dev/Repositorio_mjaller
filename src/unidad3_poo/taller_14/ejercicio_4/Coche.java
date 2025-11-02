d7346c9ad3ca74c67fc3b06b7e5c3f40a434601b
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-02 08:41:29 -0500

    feat(Coche): Agregar metodos get y set para buenas prácticas

--- src/unidad3_poo/taller_14/ejercicio_4/Coche.java
+++ src/unidad3_poo/taller_14/ejercicio_4/Coche.java
@@ -11,9 +11,38 @@ public class Coche extends Vehiculo {
         this.velocidadMaxima = velocidadMaxima;
     }
 
+    public String getMarca() {
+        return this.marca;
+    }
+    public String getModelo() {
+        return this.modelo;
+    }
+    public int getVelocidadMaxima() {
+        return this.velocidadMaxima;
+    }
+
+    public void setMarca(String marca) {
+        if (marca == null || marca.trim().isEmpty()) {
+            System.out.println("La marca no puede estar vacía, se asignará 'Desconocida' por defecto");
+            this.marca = "Desconocida";
+        } else {this.marca = marca;}
+    }
+    public void setModelo(String modelo) {
+        if (modelo == null || modelo.trim().isEmpty()) {
+            System.out.println("El modelo no puede estar vacío, se asignará 'Desconocido' por defecto");
+            this.modelo = "Desconocido";
+        } else {this.modelo = modelo;}
+    }
+    public void setVelocidadMaxima(int velocidadMaxima) {
+        if (velocidadMaxima < 0) {
+            System.out.println("La velocidad máxima es inválida, se asignará 100 por defecto");
+            this.velocidadMaxima = 160;
+        } else {this.velocidadMaxima = velocidadMaxima;}
+    }
+
     @Override
     public void mover() {
-        System.out.println("El coche " + marca + " " + modelo + " se está moviendo a una velocidad máxima de " + velocidadMaxima + " km/h");
+        System.out.println("El coche " + getMarca() + " " + getModelo() + " se está moviendo a una velocidad máxima de " + getVelocidadMaxima() + " km/h");
     }
 
 }
