63341ddc286a97ed4cc6d38412abee4fe3ecb501
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-01 19:23:17 -0500

    feat(Persona): Taller 14 - Ejercicio 2, crear clase Persona con metodo presentarse

---
+++ src/unidad3_poo/taller_14/ejercicio_2/Persona.java
@@ -0,0 +1,11 @@
+package taller_14.ejercicio_2;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String profesion;
+
+    public void presentarse() {
+        System.out.println("Hola, mi nombre es" + nombre + ", tengo " + edad + " años y soy " + profesion + ".");
+    }
+}
