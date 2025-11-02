35279c4401846eb33d55e65f963637914f64c754
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-02 09:02:30 -0500

    feat(Main): Crear clase Main para instanciar objetos de coche y Bicicleta guardados en clase Vehículo

---
+++ src/unidad3_poo/taller_14/ejercicio_4/Main.java
@@ -0,0 +1,14 @@
+package taller_14.ejercicio_4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Vehiculo vehiculo1 = new Coche("Toyota", "Prado", 180);
+        Vehiculo vehiculo2 = new Bicicleta("Giant", "Montaña", 80);
+        vehiculo1.mover();
+        vehiculo2.mover();
+
+    }
+
+}
