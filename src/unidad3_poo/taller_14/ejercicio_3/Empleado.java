3393fbb93d33634fa2131b5567374302860b8766
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-01 19:59:54 -0500

    feat(Empleado): Crear clase Empleado que hera de Persona con constructor y métodos

---
+++ src/unidad3_poo/taller_14/ejercicio_3/Empleado.java
@@ -0,0 +1,32 @@
+package taller_14.ejercicio_3;
+
+public class Empleado extends Persona {
+    private String departamento;
+
+    public Empleado(String nombre, int edad, String departamento) {
+        super(nombre, edad);
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento() {
+        return this.departamento;
+    }
+    public void setDepartamento(String departamento) {
+        if (departamento == null || departamento.trim().isEmpty()) {
+            System.out.println("El departamento no puede estar vacío, se asignará 'General' por defecto");
+            this.departamento = "General";
+        } else {this.departamento = departamento;}
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("""
+        
+        Empleado: 
+        Nombre: %s
+        Edad: %d
+        Departamento: %s
+        
+        """.formatted(getNombre(), getEdad(), getDepartamento()));
+    }
+
+}
