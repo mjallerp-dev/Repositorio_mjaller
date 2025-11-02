9b6ef2b3ef223c926addc417a1adc5cfa062207a
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-01 19:26:21 -0500

    feat(Persona): Agregar constructor, metodos get y set, actualizar metodo presentarse

--- src/unidad3_poo/taller_14/ejercicio_2/Persona.java
+++ src/unidad3_poo/taller_14/ejercicio_2/Persona.java
@@ -5,7 +5,42 @@ public class Persona {
     private int edad;
     private String profesion;
 
+    public Persona(String nombre, int edad, String profesion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.profesion = profesion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre==null || nombre.trim().isEmpty()) {
+            System.out.println("El nombre no puede estar vacío, se asignará 'Desconocido' por defecto");
+            this.nombre = "Desconocido";
+        } else {this.nombre = nombre;}
+    }
+    public void setEdad(int edad) {
+        if (edad < 0) {
+            System.out.println("La edad no puede ser negativa, se asignará 0 por defecto");
+            this.edad = 0;
+        } else {this.edad = edad;}
+    }
+    public void setProfesion(String profesion) {
+        if (profesion==null || profesion.trim().isEmpty()) {
+            System.out.println("La profesión no puede estar vacía, se asignará 'Desconocido' por defecto");
+            this.profesion = "Desconocido";
+        } else {this.profesion = profesion;}
+    }
+
     public void presentarse() {
-        System.out.println("Hola, mi nombre es" + nombre + ", tengo " + edad + " años y soy " + profesion + ".");
+        System.out.println("Hola, mi nombre es" + getNombre() + ", tengo " + getEdad() + " años y soy " + getProfesion() + ".");
     }
 }
