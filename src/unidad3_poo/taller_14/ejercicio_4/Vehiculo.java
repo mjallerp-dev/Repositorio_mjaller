0339934f35f25504f0d41ffdb51fb2dcb1fa4100
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-02 08:58:41 -0500

    feat(Vehiculo): Actualizar métodos

--- src/unidad3_poo/taller_14/ejercicio_4/Vehiculo.java
+++ src/unidad3_poo/taller_14/ejercicio_4/Vehiculo.java
@@ -1,6 +1,33 @@
 package taller_14.ejercicio_4;
 
 public class Vehiculo {
+    private String marca;
+    private int velocidadMaxima;
+
+    public Vehiculo(String marca, int velocidadMaxima) {
+        this.marca = marca;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+    public int getVelocidadMaxima() {
+        return this.velocidadMaxima;
+    }
+
+    public void setMarca(String marca) {
+        if (marca == null || marca.trim().isEmpty()) {
+            System.out.println("La marca no puede estar vacía, se asignará 'Desconocida' por defecto");
+            this.marca = "Desconocida";
+        } else {this.marca = marca;}
+    }
+    public void setVelocidadMaxima(int velocidadMaxima) {
+        if (velocidadMaxima < 0) {
+            System.out.println("La velocidad máxima es inválida, se asignará 100 por defecto");
+            this.velocidadMaxima = 160;
+        } else {this.velocidadMaxima = velocidadMaxima;}
+    }
 
     public void mover() {
         System.out.println("El vehículo se está moviendo");
