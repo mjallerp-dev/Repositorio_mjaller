d8874dfbc827be9d38c1c318b33f65a5ab0a506e
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-02 14:22:54 -0500

    feat(ReporteFunction): Crear clase con responsabilidad específica para palicar SRP

---
+++ src/unidad4_poo/taller_15/ejercicio_1/ReporteFunction.java
@@ -0,0 +1,15 @@
+package unidad4_poo.taller_15.ejercicio_1;
+
+public class ReporteFunction {
+
+        public void generarReporte(Libro libro) {
+        System.out.println("""
+        Reporte del Libro:
+
+        Título: %s
+        Autor: %s
+        Año de Publicación: %d
+        """.formatted(libro.getTitulo(), libro.getAutor(), libro.getañoDePublicacion()));
+    }
+
+}
