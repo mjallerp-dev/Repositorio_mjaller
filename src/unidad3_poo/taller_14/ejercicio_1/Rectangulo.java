7894ea35b90409ffb6d013e16a8deef1f9b67ffe
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-01 19:10:35 -0500

    feat(Rectangulo): Crear clase Rectangulo que hereda de figura y sobreescribir método

---
+++ src/unidad3_poo/taller_14/ejercicio_1/Rectangulo.java
@@ -0,0 +1,41 @@
+package taller_14.ejercicio_1;
+
+public class Rectangulo extends Figura {
+
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+        public double getAltura() {
+        return altura;
+    }
+
+    public void setBase(double base) {
+        if (base <= 0) {
+            System.out.println("El valor de la base es inválido, se asignará 1 por defecto");
+            this.base = 1;
+        }
+        else{this.base = base;}
+    }
+
+    public void setAltura(double altura) {
+        if (altura <= 0) {
+            System.out.println("El valor de la altura es inválido, se asignará 1 por defecto");
+            this.altura = 1;
+        }
+        else{this.altura = altura;}
+    }
+
+    @Override
+    public void calcularArea() {
+        System.out.println("El area del rectangulo es: " + (base * altura));
+    }
+
+}
