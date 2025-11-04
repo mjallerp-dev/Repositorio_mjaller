e0469744de279b837fc6050c9f3d4fd8b86b9f74
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-03 23:26:26 -0500

    feat(DescuentoFijo): Crear clase DescuentoDijo con constructor y métodos

---
+++ src/unidad4_poo/taller_16/DescuentoFijo.java
@@ -0,0 +1,32 @@
+package unidad4_poo.taller_16;
+
+public class DescuentoFijo {
+    private double monto;
+
+    public DescuentoFijo(double precio, double monto) {
+        this.monto = monto;
+    }
+
+    public double getMonto() {
+        return this.monto;
+    }
+    
+    public void setMonto(double monto) {
+        if (monto < 0) {
+            System.out.println("El monto a descontar no puede ser negativo, se asignará 0 por defecto.");
+            this.monto = 0;
+        } else {
+            this.monto = monto;
+        }
+    }
+
+    public void aplicarDescuento(double precio) {
+        double precioFinal = precio - getMonto();
+        if (precioFinal < 0) {
+            System.out.println("El descuento excede el precio, el precio final se ajustará a 0.");
+            precioFinal = 0;
+        }
+        System.out.println("Precio con descuento fijo de $" + getMonto() + ": $" + precioFinal);
+    }
+
+}
