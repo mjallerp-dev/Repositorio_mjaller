301c5cef1cd3942f38e6256f781c0bad3829e268
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-03 21:44:37 -0500

    feat(Main): Crear método Main para instanciar objeto de usuario y probar clases de los Servicios

---
+++ src/unidad4_poo/taller_15/ejercicio_3/Main.java
@@ -0,0 +1,34 @@
+package unidad4_poo.taller_15.ejercicio_3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Usuario usuario1 = new Usuario("admin@mail.com", "12345678");
+
+        ValidationService validationService = new ValidationService();
+        AutenticationService autenticationService = new AutenticationService();
+
+        if (validationService.validar(usuario1)){
+            if (autenticationService.autenticar(usuario1)) {
+                System.out.println("""
+                    
+                Autenticación exitosa
+                
+                Bienvenido/a al sistema %s
+
+                """.formatted(usuario1.getEmail()));
+            } else {
+                System.out.println("Error de autenticación: Email y/o contraseña incorrecto/a.");
+            }
+        } else {
+            System.out.println("""
+
+                Email y/o contraseña inválido/a.
+
+                El email debe contener '@' y '.'.
+                La contraseña debe tener al menos 8 caracteres.
+
+                """);
+        }
+    }
+}
