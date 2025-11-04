2a6eb37946d67da80bb1921231b820f8336ec161
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-03 23:15:36 -0500

    feat(Descuento): Agregar atributo precio y modificar método aplicarDescuento

--- src/unidad4_poo/taller_16/Descuento.java
+++ src/unidad4_poo/taller_16/Descuento.java
@@ -1,7 +1,23 @@
 package unidad4_poo.taller_16;
 
 public abstract class Descuento {
+    private double precio;
 
-    public abstract void aplicarDescuento(double precioFinal);
+    public Descuento(double precio) {
+        this.precio = precio;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+    public void setPrecio(double precio) {
+        if (precio < 0) {
+            System.out.println("El precio no puede ser negativo, transacción cancelada.");
+        } else {
+            this.precio = precio;
+        }
+    }
+ 
+    public abstract void aplicarDescuento();
     
 }
