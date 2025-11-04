8d82897d467216733842d9bc04dc0e8305d04cc8
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-03 21:43:19 -0500

    feat(ValidationService): Redefinir método validar

--- src/unidad4_poo/taller_15/ejercicio_3/ValidationService.java
+++ src/unidad4_poo/taller_15/ejercicio_3/ValidationService.java
@@ -3,19 +3,6 @@ package unidad4_poo.taller_15.ejercicio_3;
 public class ValidationService {
 
     public boolean validar(Usuario u) {
-        if (u.getEmail().contains("@") && u.getEmail().contains(".") && u.getcontrasena().length() >= 8) {
-            return true;
-        } else{
-            if (!u.getEmail().contains("@") && !u.getEmail().contains(".") && u.getcontrasena().length() < 8) {
-                System.out.println("Email y/o contrasena inválido/a.");
-            }
-            else if (!u.getEmail().contains("@") && !u.getEmail().contains(".")) {
-                System.out.println("Error: Email inválido, debe contener '@' y '.'");
-            }
-            else if (u.getcontrasena().length() < 8) {
-                System.out.println("Error: contrasena inválida, debe tener al menos 8 caracteres.");
-            }
-            return false;
-        }
+        return u.getEmail().contains("@") && u.getEmail().contains(".") && u.getcontrasena().length() >= 8;
     }
 }
