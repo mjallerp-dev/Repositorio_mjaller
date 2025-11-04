4a1915a5e2b0d9014849b4795c96da6cd66f6539
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-03 23:27:02 -0500

    feat(DescuentoFijo): Aplicar correciones al código

--- src/unidad4_poo/taller_16/DescuentoFijo.java
+++ src/unidad4_poo/taller_16/DescuentoFijo.java
@@ -1,16 +1,17 @@
 package unidad4_poo.taller_16;
 
-public class DescuentoFijo {
+public class DescuentoFijo extends Descuento {
     private double monto;
 
     public DescuentoFijo(double precio, double monto) {
+        super(precio);
         this.monto = monto;
     }
 
     public double getMonto() {
         return this.monto;
     }
-    
+
     public void setMonto(double monto) {
         if (monto < 0) {
             System.out.println("El monto a descontar no puede ser negativo, se asignará 0 por defecto.");
@@ -20,13 +21,13 @@ public class DescuentoFijo {
         }
     }
 
-    public void aplicarDescuento(double precio) {
-        double precioFinal = precio - getMonto();
+    @Override
+    public void aplicarDescuento() {
+        double precioFinal = getPrecio() - getMonto();
         if (precioFinal < 0) {
             System.out.println("El descuento excede el precio, el precio final se ajustará a 0.");
             precioFinal = 0;
         }
         System.out.println("Precio con descuento fijo de $" + getMonto() + ": $" + precioFinal);
     }
-
 }
