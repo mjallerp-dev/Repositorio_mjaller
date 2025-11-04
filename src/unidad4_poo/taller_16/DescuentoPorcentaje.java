97d5cc80715a48de74334a433290ec113c43befe
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-03 23:20:49 -0500

    feat(DescuentoPorcentaje): Agregar métodos get y set

--- src/unidad4_poo/taller_16/DescuentoPorcentaje.java
+++ src/unidad4_poo/taller_16/DescuentoPorcentaje.java
@@ -1,16 +1,28 @@
 package unidad4_poo.taller_16;
 
 public class DescuentoPorcentaje extends Descuento {
-    private final double porcentaje;
+    private double porcentaje;
 
     public DescuentoPorcentaje(double precio, double porcentaje) {
         super(precio);
         this.porcentaje = porcentaje;
     }
 
+    public double getPorcentaje() {
+        return this.porcentaje;
+    }
+    public void setPorcentaje(double porcentaje) {
+        if (porcentaje < 0 || porcentaje > 100) {
+            System.out.println("El porcentaje debe estar entre 0 y 100, se asignará 0 por defecto.");
+            this.porcentaje = 0;
+        } else {
+            this.porcentaje = porcentaje;
+        }
+    }
+
     @Override
     public void aplicarDescuento() {
-        double precioFinal = getPrecio() - (getPrecio() * this.porcentaje / 100);
-        System.out.println("Precio con descuento del " + this.porcentaje + "%: " + precioFinal);
+        double precioFinal = getPrecio() - (getPrecio() * getPorcentaje() / 100);
+        System.out.println("Precio con descuento del " + getPorcentaje() + "%: $" + precioFinal);
     }
 }
