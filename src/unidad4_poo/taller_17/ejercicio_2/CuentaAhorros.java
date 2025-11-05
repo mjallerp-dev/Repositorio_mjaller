a30cff101d111bbdec39198aa03dd515f96e8fb1
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 14:53:10 -0500

    feat(CuentaAhorros): Actualizar lógica de metodos para cumplir con el LSP

--- src/unidad4_poo/taller_17/ejercicio_2/CuentaAhorros.java
+++ src/unidad4_poo/taller_17/ejercicio_2/CuentaAhorros.java
@@ -3,18 +3,31 @@ package unidad4_poo.taller_17.ejercicio_2;
 public class CuentaAhorros extends CuentaBancaria {
     private final double tasaInteres;
 
-    public CuentaAhorros(String titular, String numeroCuenta, double saldo,  double monto, double tasaInteres) {
+    public CuentaAhorros(String titular, String numeroCuenta, double saldo, double monto) {
         super(titular, numeroCuenta, saldo, monto);
-        this.tasaInteres = 3;
+        this.tasaInteres = 1.5; // Tasa de interés fija del 1.5% mensual
     }
 
     public double getTasaInteres() {
         return this.tasaInteres;
     }
 
+    public void InteresGenerado() {
+        System.out.println("""
+            
+            Calculando interés generado...
+
+            Saldo actual: %.2f
+            Tasa de interés mensual: %.2f%%
+
+            Interés generado: %.2f
+
+            """.formatted(getSaldo(), getTasaInteres(), (getSaldo() * tasaInteres) / 100));
+    }
+    
     @Override
     public void Transferir() {
-        double saldoactualizado = getSaldo() - getMonto() - (getMonto() * getTasaInteres() / 100);
+        double saldoActualizado = getSaldo() - getMonto();
         System.out.println("""
             
             Saldo actual: %.2f
@@ -24,11 +37,10 @@ public class CuentaAhorros extends CuentaBancaria {
             Número de cuenta: %s
             Titular: %s
             Monto: %.2f
-            Tasa de interés: %.2f%
-
+            
             Saldo actualizado: %.2f
 
-            """.formatted(getSaldo(), getNumeroCuenta(), getTitular(), getMonto(), getTasaInteres(), saldoactualizado));
+            """.formatted(getSaldo(), getNumeroCuenta(), getTitular(), getMonto(), saldoActualizado));
     }
 
 }
