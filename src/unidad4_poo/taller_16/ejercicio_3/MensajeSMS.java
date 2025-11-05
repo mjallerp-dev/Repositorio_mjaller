73d3b19630ba0f131c331f778598e26dd1671b08
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-04 22:15:55 -0500

    feat(MensajeSMS): Crear clase para enviar mensajes por Mensaje de Texto SMS

---
+++ src/unidad4_poo/taller_16/ejercicio_3/MensajeSMS.java
@@ -0,0 +1,10 @@
+package unidad4_poo.taller_16.ejercicio_3;
+
+public class MensajeSMS extends Mensajero {
+
+    @Override
+    public void enviar(String mensaje) {
+        System.out.println("Enviando SMS: " + mensaje);
+    }
+
+}
