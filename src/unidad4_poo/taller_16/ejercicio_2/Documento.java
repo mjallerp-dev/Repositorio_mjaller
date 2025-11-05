1f49a1fbe338b9bae5cee28cd62ac881b4105095
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-04 21:42:09 -0500

    feat(Documento): Agregar métodos get y set, mejorar metodo exportarPDF

--- src/unidad4_poo/taller_16/ejercicio_2/Documento.java
+++ src/unidad4_poo/taller_16/ejercicio_2/Documento.java
@@ -7,11 +7,24 @@ public abstract class Documento {
         this.fileName = fileName;
     }
 
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public void setFileName(String fileName) {
+        if (fileName == null || fileName.trim().isEmpty()) {
+            System.out.println("El nombre del archivo no puede estar vacío, se asiganará 'documento' por defecto.");
+            this.fileName = "documento";
+        } else {
+            this.fileName = fileName;
+        }
+    }
+
     public void exportarPDF() {
         System.out.println("""
 
             Exportando %s como PDF...
-            PDF exportado exitosamente.
+            %s.pdf exportado exitosamente.
             """.formatted(this.fileName));
     }
 }
