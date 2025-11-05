d0500a5da0a85f055500ff4d3be667f24bd94122
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 12:50:33 -0500

    feat(CuentaBancaria): Añadir atributos y métodos

--- src/unidad4_poo/taller_17/ejercicio_2/CuentaBancaria.java
+++ src/unidad4_poo/taller_17/ejercicio_2/CuentaBancaria.java
@@ -4,19 +4,64 @@ public class CuentaBancaria {
     private String numeroCuenta;
     private double saldo;
     private String titular;
+    private double monto;
 
-    public CuentaBancaria(String numeroCuenta) {
+    public CuentaBancaria(String numeroCuenta, double saldo, String titular, double monto) {
         this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.titular = titular;
     }
     public String getNumeroCuenta() {
         return this.numeroCuenta;
     }
+    public double getSaldo() {
+        return this.saldo;
+    }
+    public String getTitular() {
+        return this.titular;
+    }
+    public double getMonto() {
+        return this.monto;
+    }
+
     public void setNumeroCuenta(String numeroCuenta) {
-        this.numeroCuenta = numeroCuenta;
+        if (numeroCuenta == null || numeroCuenta.trim().isEmpty()) {
+            System.out.println("El número de cuenta no puede estar vacío, transaccion rechazada.");
+        } else {
+        this.numeroCuenta = numeroCuenta;}
+    }
+    public void setSaldo(double saldo) {
+        if (saldo < 0) {
+            System.out.println("El saldo no puede ser negativo, transaccion rechazada.");
+        } else {
+        this.saldo = saldo;}
+    }
+    public void setTitular(String titular) {
+        if (titular == null || titular.trim().isEmpty()) {
+            System.out.println("El nombre del titular no puede estar vacío, transaccion rechazada.");
+        } else {
+        this.titular = titular;}
+    }
+    public void setMonto(double monto) {
+        if (monto < 0) {
+            System.out.println("El monto no puede ser negativo, transaccion rechazada.");
+        } else {
+        this.monto = monto;}
     }
 
     public void Transferencia(){
-        System.out.println("Realizando transferencia hacia cuenta: " + getNumeroCuenta());
+        System.out.println("""
+            
+            Realizando transferencia...
+
+            Número de cuenta: %s
+            Titular: %s
+            Monto: %.2f
+
+            Saldo actualizado: %.2f
+
+                
+            """.formatted(getNumeroCuenta(), getTitular(), getMonto(), getSaldo()));
     }
 
 }
