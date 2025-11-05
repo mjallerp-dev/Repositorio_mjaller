806dc9c1810914cf9bf9c0b3f7569c379cce814f
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-04 23:56:27 -0500

    feat(Rectangulo): Crear clase Rectangulo que hereda de Figura y sobreescribe metodo calcularArea

---
+++ src/unidad4_poo/taller_17/ejercicio_1/Rectangulo.java
@@ -0,0 +1,17 @@
+package unidad4_poo.taller_17.ejercicio_1;
+
+public class Rectangulo extends Figura {
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return this.base * this.altura;
+    }
+
+}
