a2d1b979b3abe84fdeedf9e1a940031ab4319d96
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-04 23:57:24 -0500

    feat(Rectangulo): Agregar métodos get y set

--- src/unidad4_poo/taller_17/ejercicio_1/Rectangulo.java
+++ src/unidad4_poo/taller_17/ejercicio_1/Rectangulo.java
@@ -9,9 +9,29 @@ public class Rectangulo extends Figura {
         this.altura = altura;
     }
 
+        public double getBase() {
+        return this.base;
+    }
+        public double getAltura() {
+        return this.altura;
+    }
+
+    public void setBase(double base) {
+        if (base < 0) {
+            System.out.println("La base no puede ser negativa, se asignará '1' por defecto.");
+        } else {
+            this.base = base;}
+    }
+    public void setAltura(double altura) {
+        if (altura < 0) {
+            System.out.println("La altura no puede ser negativa, se asignará '1' por defecto.");
+        } else {
+        this.altura = altura;}
+    }
+
     @Override
     public double calcularArea() {
-        return this.base * this.altura;
+        return getBase() * getAltura();
     }
 
 }
