2217811da8679c2d3295d64bfc4f2bdf299c9c51
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-04 22:11:08 -0500

    feat(Mensajero): Crear clase mensajero que envía mensajes por Email

---
+++ src/unidad4_poo/taller_16/ejercicio_3/Mensajero.java
@@ -0,0 +1,9 @@
+package unidad4_poo.taller_16.ejercicio_3;
+
+public abstract class Mensajero {
+
+    public void enviarEmail(String mensaje) {
+        System.out.println("Enviando mensaje: " + mensaje);
+    }
+
+}
