6b90ddbfe3c85c1f46e64be2869f4243d24ce371
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 20:28:32 -0500

    feat(CuentaBancaria): Crear clase que implementa la interfaz Transferencia y sobreescribir método

---
+++ src/unidad4_poo/taller_18/ejercicio_2/CuentaBancaria.java
@@ -0,0 +1,10 @@
+package unidad4_poo.taller_18.ejercicio_2;
+
+public class CuentaBancaria implements Transferencia {
+
+    @Override
+    public void transferir() {
+        System.out.println("Transfiriendo dinero a otra cuenta bancaria.");
+    }
+
+}
