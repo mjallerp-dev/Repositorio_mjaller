c10903efacae6176891606970efca69b9687f070
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 20:06:57 -0500

    feat(MantenimientoLimpieza): Crear clase que implementa interfaz Limpieza y sobreescribir metodo

---
+++ src/unidad4_poo/taller_18/MantenimientoLimpieza.java
@@ -0,0 +1,10 @@
+package unidad4_poo.taller_18;
+
+public class MantenimientoLimpieza implements Limpieza {
+
+    @Override
+    public void limpiar() {
+        System.out.println("Realizando tareas de limpieza...");
+    }
+
+}
