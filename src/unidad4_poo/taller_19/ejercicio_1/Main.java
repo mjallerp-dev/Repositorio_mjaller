d633b43c65dcb2ac76e6416d372420b7feea3812
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 23:38:55 -0500

    feat(Main): Crear clase Main para instanciar objetos y probar la dependencia

---
+++ src/unidad4_poo/taller_19/ejercicio_1/Main.java
@@ -0,0 +1,17 @@
+package unidad4_poo.taller_19.ejercicio_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        ServicioAutenticacion autenticacionLocal = new AutenticacionLocal();
+        GestorAutenticacion gestorAutenticacion = new GestorAutenticacion(autenticacionLocal);
+        gestorAutenticacion.autenticar("admin@mail.com", "12345678");
+
+        ServicioAutenticacion autenticacionOAuth = new AutentiacionOAuth();
+        gestorAutenticacion = new GestorAutenticacion(autenticacionOAuth);
+        gestorAutenticacion.autenticar("admin@google.com","123456");
+
+    }
+
+}
