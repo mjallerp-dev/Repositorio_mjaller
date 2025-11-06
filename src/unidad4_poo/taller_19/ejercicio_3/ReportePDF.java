061b2f3622388b434997be9229a0be7429f08ef2
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-06 00:20:53 -0500

    feat(ReportePDF): Crear clase que implemente la interfaz GeneradorReporte y sobreescribir metodo

---
+++ src/unidad4_poo/taller_19/ejercicio_3/ReportePDF.java
@@ -0,0 +1,10 @@
+package unidad4_poo.taller_19.ejercicio_3;
+
+public class ReportePDF implements GeneradorReporte {
+
+    @Override
+    public void generarReporte(String nombreReporte){
+        System.out.println("Reporte " + nombreReporte + ".pdf generado con éxito");
+    }
+
+}
