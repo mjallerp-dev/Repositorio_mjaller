091c2ba73e204282a73034ead8eca463979265e3
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 23:13:04 -0500

    feat(AutenticacionLocal): crear Clase que implementa la interfaz para usar metodo de autenticacion de usuario

---
+++ src/unidad4_poo/taller_19/ejercicio_1/AutenticacionLocal.java
@@ -0,0 +1,18 @@
+package unidad4_poo.taller_19.ejercicio_1;
+
+public class AutenticacionLocal implements ServicioAutenticacion {
+
+    @Override
+    public boolean autenticarUsuario(String email, String contrasena){
+        if ("admin@mail.com".equals(email) && "12345678".equals(contrasena)){
+            System.out.println("Autenticación exitosa.");
+            return true;
+        }
+        else {
+            System.out.println("Autenticacion fallida.");
+            return false;
+        }
+        
+    }   
+
+}
--- src/unidad4_poo/taller_19/ejercicio_1/ServicioAutenticacion.java
+++ src/unidad4_poo/taller_19/ejercicio_1/ServicioAutenticacion.java
@@ -2,5 +2,5 @@ package unidad4_poo.taller_19.ejercicio_1;
 
 public interface ServicioAutenticacion {
 
-    void autenticarUsuario();
+    boolean autenticarUsuario(String email, String contrasena);
 }
