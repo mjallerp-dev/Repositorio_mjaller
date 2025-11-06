21b848982316d155b0db73f47785c2d19f5222a3
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 20:29:11 -0500

    feat(Cajero): Crear clase que implementa interfaz Transferencia y sobreescribir metodo

---
+++ src/unidad4_poo/taller_18/ejercicio_2/Cajero.java
@@ -0,0 +1,10 @@
+package unidad4_poo.taller_18.ejercicio_2;
+
+public class Cajero implements Retiro {
+
+    @Override
+    public void retirar() {
+        System.out.println("Realizando retiro desde el Cajero Automático...");
+    }
+
+}
