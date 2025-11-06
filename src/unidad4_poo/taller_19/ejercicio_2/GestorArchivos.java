ff598b427803e2e04e65216da99d0178e052e0c6
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 23:55:08 -0500

    feat(GestorArchivos): Crear clase Gestor Archivos que depende de interfaz Almacenamiento

---
+++ src/unidad4_poo/taller_19/ejercicio_2/GestorArchivos.java
@@ -0,0 +1,13 @@
+package unidad4_poo.taller_19.ejercicio_2;
+
+public class GestorArchivos {
+    private Almacenamiento almacenamiento;
+
+    public GestorArchivos(Almacenamiento almacenamiento) {
+        this.almacenamiento = almacenamiento;
+    }
+
+    public void generar(String tipoReporte, String carpetaDestino){
+        almacenamiento.generarReporte(tipoReporte, carpetaDestino);
+    }
+}
