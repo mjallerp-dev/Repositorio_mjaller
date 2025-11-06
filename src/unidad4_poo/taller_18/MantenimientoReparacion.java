af4400cba9dc0513b7e5f3a6bfc2d0ce3897317c
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-05 20:07:53 -0500

    feat(MantenimientoReparacion): Crear clase que implementa la interfaz Reparacion y sobreescribir metodo

---
+++ src/unidad4_poo/taller_18/MantenimientoReparacion.java
@@ -0,0 +1,10 @@
+package unidad4_poo.taller_18;
+
+public class MantenimientoReparacion implements Reparacion {
+
+    @Override
+    public void reparar() {
+        System.out.println("Realizando tareas de reparación...");
+    }
+
+}
