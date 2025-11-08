f05236301e081aae49500750534824620ed797b4
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-07 21:14:20 -0500

    feat(Perro): Evaluar mala práctica creando clase Perro que implementa la interfaz Volador y sobreescribe el metodo volar

---
+++ src/unidad3_poo/taller_12/ejercicio_3_1/Perro.java
@@ -0,0 +1,10 @@
+package taller_12.ejercicio_3_1;
+
+public class Perro implements Volador{
+
+    @Override
+    public void volar() {
+        System.out.println("El perro no puede volar");
+    }  
+
+}
