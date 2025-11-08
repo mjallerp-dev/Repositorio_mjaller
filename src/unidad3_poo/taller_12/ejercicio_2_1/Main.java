9d1bd07cf5f8c380d69f6a6cb755d775c40e6674
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-07 21:05:14 -0500

    feat(Main): Implementar clase Main para instanciar y probar métodos de Animal

---
+++ src/unidad3_poo/taller_12/ejercicio_2_1/Main.java
@@ -0,0 +1,12 @@
+package taller_12.ejercicio_2_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+        animal.nadar();
+        animal.volar();
+    }
+
+}
