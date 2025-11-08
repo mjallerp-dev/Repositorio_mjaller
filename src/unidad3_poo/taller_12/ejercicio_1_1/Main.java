1e387166937e47fa910993e1b4cca7091fe1e75c
mjallerp-dev <mjallerp@unicartagena.edu.co>
2025-11-07 21:01:35 -0500

    feat(Main): Crear clase Main para instanciar objetos de Persona y probar métodos

---
+++ src/unidad3_poo/taller_12/ejercicio_1_1/Main.java
@@ -0,0 +1,13 @@
+package taller_12.ejercicio_1_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona persona = new Persona();
+        persona.hablar();
+        persona.trabajar();
+    }
+
+
+}
