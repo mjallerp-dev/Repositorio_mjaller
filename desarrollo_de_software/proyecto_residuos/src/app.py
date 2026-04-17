1ba937494ed1c4303697526780e3a350a8ef6d55
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-04-17 00:55:00 -0500

    feat(HU_03): Modificacion del insert para añadir carga de documentos con validación de formato

--- desarrollo_de_software/proyecto_residuos/src/app.py
+++ desarrollo_de_software/proyecto_residuos/src/app.py
@@ -1,59 +1,70 @@
 import os
-import psycopg2
-import bcrypt
-from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from supabase import create_client
+from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+
 app = Flask(__name__)
 CORS(app)
 
-def conexion_bd():
-    return psycopg2.connect(
-        host=os.getenv('DB_HOST'),
-        database=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASS'),
-        port=os.getenv('DB_PORT')
-    )
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+formato_permitido = {'pdf', 'docx'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in formato_permitido
 
 @app.route('/registrar_empresa', methods=['POST'])
-def registrar_empresa():
-    data = request.get_json()
-
-    nit = data.get('nit')
-    razon_social = data.get('razon_social')
-    correo = data.get('correo')
-    password_hash = data.get('password')
-    rol = data.get('rol')
-
-    #Usamos la librería bcrypt para el cifrado de contraseña
-    salt = bcrypt.gensalt()
-    password_hash = bcrypt.hashpw(password_hash.encode('utf-8'), salt).decode('utf-8')
-
-    conn = None
+def registrar():
     try:
-        conn = conexion_bd()
-        cur = conn.cursor()
+ 
+        nit = request.form.get('nit')
+        razon_social = request.form.get('razon_social')
+        correo = request.form.get('correo')
+        password = request.form.get('password')
+        rol = request.form.get('rol')
 
-        query = """
-        INSERT INTO enterprise (nit, razon_social, correo, password, rol)
-        VALUES (%s, %s, %s, %s, %s);
-        """
-        cur.execute(query, (nit, razon_social, correo, password_hash, rol))
+        archivo = request.files.get('documento_legal')
+
+        if archivo and not allowed_file(archivo.filename):
+            return jsonify({"error": "Formato de archivo no permitido. Use PDF o DOCX"}), 400
+
+        if not archivo:
+            return jsonify({"error": "Falta la documentacion legal"}), 400
+
+        nombre_archivo = f"{nit}_{archivo.filename}"
+
+        temp_path = f"./{nombre_archivo}"
+        archivo.save(temp_path)
+
+        with open(temp_path, 'rb') as f:
+            supabase.storage.from_("documentos_legales").upload(nombre_archivo, f)
         
-        conn.commit()
-        return jsonify({"message": "Registro exitoso", "status": "success"}), 201
+        url_publica = supabase.storage.from_("documentos_legales").get_public_url(nombre_archivo)
 
-    except psycopg2.IntegrityError:
-        return jsonify({"message": "El NIT o el Correo ya están registrados"}), 400
+        os.remove(temp_path)
+
+        datos_registro = {
+            "nit": nit,
+            "razon_social": razon_social,
+            "correo": correo,
+            "password": password,
+            "rol": rol,
+            "url_documento": url_publica
+        }
+        
+        res = supabase.table("empresas").insert(datos_registro).execute()
+
+        return jsonify({"mensaje": "Solicitud de registro enviada exitosamente."}), 200
+
     except Exception as e:
-        return jsonify({"message": str(e), "status": "error"}), 500
-    finally:
-        if conn:
-            cur.close()
-            conn.close()
+        print(f"Error: {e}")
+        return jsonify({"error": "Ocurrió un error en el servidor"}), 500
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
